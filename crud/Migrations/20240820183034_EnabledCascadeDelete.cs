1c66b79a30e6cfa05575dbeb8c610d8cbd912632
Akhil Goud Burra <124227421+Akhil-Goud-Burra@users.noreply.github.com>
2024-08-20 14:37:09 -0400

    Implemented Delete Opperation

---
+++ crud/Migrations/20240820183034_EnabledCascadeDelete.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace crud.Migrations
+{
+    /// <inheritdoc />
+    public partial class EnabledCascadeDelete : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "EmployeeTable",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Firstname = table.Column<string>(type: "varchar(50)", unicode: false, maxLength: 50, nullable: true),
+                    Lastname = table.Column<string>(type: "varchar(50)", unicode: false, maxLength: 50, nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_EmployeeTable", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "JobDescription",
+                columns: table => new
+                {
+                    JobDescriptionId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    JobTitle = table.Column<string>(type: "varchar(50)", unicode: false, maxLength: 50, nullable: true),
+                    StartDate = table.Column<DateTime>(type: "datetime", nullable: false),
+                    EndDate = table.Column<string>(type: "varchar(50)", unicode: false, maxLength: 50, nullable: true),
+                    EmployeeId = table.Column<int>(type: "int", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_JobDescription", x => x.JobDescriptionId);
+                    table.ForeignKey(
+                        name: "FK_JobDescription_EmployeeTable",
+                        column: x => x.EmployeeId,
+                        principalTable: "EmployeeTable",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_JobDescription_EmployeeId",
+                table: "JobDescription",
+                column: "EmployeeId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "JobDescription");
+
+            migrationBuilder.DropTable(
+                name: "EmployeeTable");
+        }
+    }
+}
---
+++ crud/Models/EmoloyeeModelDTO/EmployeeDeleteDTO.cs
@@ -0,0 +1,10 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace crud.Models.EmoloyeeModelDTO
+{
+    public class EmployeeDeleteDTO
+    {
+        [Required]
+        public int Id { get; set; }
+    }
+}
