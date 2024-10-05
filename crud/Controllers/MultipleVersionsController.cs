12e00fe8d09ad9fe11749174315c71535f88e9c0
Akhil Goud Burra <124227421+Akhil-Goud-Burra@users.noreply.github.com>
2024-10-05 17:48:28 -0500

    Multiple Versions in  controller using Query String with Swagger

---
+++ crud/Controllers/MultipleVersionsController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace crud.Controllers
+{
+    [ApiController]
+    [Route("api/MultipleVersions")]
+    [ApiVersion("1.0")]
+    [ApiVersion("2.0")]
+
+    public class MultipleVersionsController : Controller
+    {
+        [HttpGet]
+        [MapToApiVersion("1.0")]
+        public IActionResult GetV1()
+        {
+            return new OkObjectResult("employees from v1 Method");
+        }
+
+
+
+        [HttpGet]
+        [MapToApiVersion("2.0")]
+        public IActionResult GetV2()
+        {
+            return new OkObjectResult("employees from v2 Method");
+        }
+    }
+}
