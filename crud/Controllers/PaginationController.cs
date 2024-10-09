c86ce7dc6d6a5e3cbc713abdb76a9f77f7437afa
Akhil Goud Burra <124227421+Akhil-Goud-Burra@users.noreply.github.com>
2024-10-08 21:25:23 -0500

    Pagination

---
+++ crud/Controllers/PaginationController.cs
@@ -0,0 +1,43 @@
+﻿using crud.Models;
+using crud.Models.PaginationParams;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System;
+
+namespace crud.Controllers
+{
+    [ApiController]
+    [Route("api/PaginatedData")]
+    public class PaginationController : Controller
+    {
+
+        private readonly MyDbContext appDbContext;
+
+        public PaginationController(MyDbContext appDbContext)
+        {
+            this.appDbContext = appDbContext;
+        }
+
+
+        // Http Get Request
+        [HttpGet]
+        public ActionResult<IEnumerable<EmployeeTable>> GetAllEmployees([FromQuery] Pagination pagination)
+        {
+            var All_Employees = appDbContext.EmployeeTables
+                                    .Include(e => e.JobDescriptions) // This will include JobDescriptions in the query
+                                    .ToList();
+
+            // Apply Pagination
+            var pagedEmployees = All_Employees.Skip((pagination.PageNumber - 1) * pagination.PageSize).Take(pagination.PageSize).ToList();
+
+
+            if (pagedEmployees == null || !pagedEmployees.Any())
+            {
+                return NotFound(new { Message = "No employees found in the DataBase" });
+            }
+
+            return Ok(pagedEmployees);
+        }
+    }
+}
---
+++ crud/Models/PaginationParams/Pagination.cs
@@ -0,0 +1,8 @@
+﻿namespace crud.Models.PaginationParams
+{
+    public class Pagination
+    {
+        public int PageSize { get; set; }
+        public int PageNumber { get; set; }
+    }
+}
